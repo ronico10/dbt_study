4ec05af106db9bcf899e81a7475f71485babfec7
Toru Matsushita <14ee1302@gmail.com>
2026-05-17 07:25:37 +0000

    add current_timestamp

--- models/connected_summary.sql
+++ models/connected_summary.sql
@@ -10,7 +10,8 @@ SELECT
     {{ check_main_warehouse('master.WAREHOUSE_CODE') }} as warehouse_mark,
     COALESCE(receipt.total_received_qty, 0) - COALESCE(shipment.total_shipped_qty, 0) as net_movement,
     CASE WHEN shipment.total_shipped_qty IS NOT NULL THEN TRUE ELSE FALSE END AS has_shipment,
-    CASE WHEN receipt.total_received_qty IS NOT NULL THEN TRUE ELSE FALSE END AS has_receipt
+    CASE WHEN receipt.total_received_qty IS NOT NULL THEN TRUE ELSE FALSE END AS has_receipt,
+    CURRENT_TIMESTAMP() as updated_at
 FROM {{ source('demo', 'product_master') }} as master
 LEFT JOIN {{ ref('shipment') }}
 ON master.product_code = shipment.product_code
