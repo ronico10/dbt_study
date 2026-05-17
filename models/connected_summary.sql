d9ca7a0a8b765c147450e1ef33ee83507fac5fe2
Toru Matsushita <14ee1302@gmail.com>
2026-05-17 06:52:44 +0000

    connected_summary.sql created

--- models/connected_summary.sql
+++ models/connected_summary.sql
@@ -6,6 +6,8 @@ SELECT
     shipment.total_shipped_qty,
     receipt.total_received_qty,
     inventory.total_inventory_qty,
+    {{ get_specific_category('CATEGORY') }} as cat_label,
+    {{ check_main_warehouse('master.WAREHOUSE_CODE') }} as warehouse_mark,
     COALESCE(receipt.total_received_qty, 0) - COALESCE(shipment.total_shipped_qty, 0) as net_movement,
     CASE WHEN shipment.total_shipped_qty IS NOT NULL THEN TRUE ELSE FALSE END AS has_shipment,
     CASE WHEN receipt.total_received_qty IS NOT NULL THEN TRUE ELSE FALSE END AS has_receipt
