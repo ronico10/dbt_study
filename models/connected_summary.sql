8b07daeda9e91c4fcc1627c1534e282f178c0617
Toru Matsushita <14ee1302@gmail.com>
2026-05-17 05:19:02 +0000

    create models

---
+++ models/connected_summary.sql
@@ -0,0 +1,21 @@
+SELECT
+    master.PRODUCT_CODE,
+    master.PRODUCT_NAME,
+    CATEGORY,
+    master.WAREHOUSE_CODE,
+    shipment.total_shipped_qty,
+    receipt.total_received_qty,
+    inventory.total_inventory_qty,
+    COALESCE(receipt.total_received_qty, 0) - COALESCE(shipment.total_shipped_qty, 0) as net_movement,
+    CASE WHEN shipment.total_shipped_qty IS NOT NULL THEN TRUE ELSE FALSE END AS has_shipment,
+    CASE WHEN receipt.total_received_qty IS NOT NULL THEN TRUE ELSE FALSE END AS has_receipt
+FROM {{ source('demo', 'product_master') }} as master
+LEFT JOIN {{ ref('shipment') }}
+ON master.product_code = shipment.product_code
+AND master.warehouse_code = shipment.warehouse_code
+LEFT JOIN {{ ref('receipt') }}
+ON master.product_code = receipt.product_code
+AND master.warehouse_code = receipt.warehouse_code
+LEFT JOIN {{ ref('inventory') }}
+ON master.product_code = inventory.product_code
+AND master.warehouse_code = inventory.warehouse_code
